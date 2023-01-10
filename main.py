891f0644c77c9ee583c2043b8c6f1351e775b32e
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-10 17:04:38 +0200

    created process_coins function

--- main.py
+++ main.py
@@ -34,12 +34,22 @@ resources = {
 
 
 def is_resource_sufficient(order_ingredients):
-    '''Checks if coffee machine has enough resources to prepare selected drink'''
+    '''Returns True if order can be prepared with existing resources, False if not enough resources'''
     for item in order_ingredients:
         if order_ingredients[item] > resources[item]:
             print(f"Not enough {item}")
             return False
     return True
+
+
+def process_coins():
+    '''Takes coins and returns total amount of money inserted'''
+    print("Please insert coins")
+    total = int(input("How many quarters?\n")) * 0.25  # value of 1 quarter
+    total += int(input("How many dimes?\n")) * 0.1  # value of 1 dime
+    total += int(input("How many nickels?\n")) * 0.05  # value of 1 nickel
+    total += int(input("How many pennies?\n")) * 0.01  # value of 1 penny
+    return total
 
 
 is_on = True

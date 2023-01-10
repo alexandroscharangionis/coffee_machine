c4e6fdb8d905c930d262e4696048f0f87be6572f
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-10 16:30:27 +0200

    added is_on boolean for coffee machine while loop

--- main.py
+++ main.py
@@ -29,3 +29,10 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+is_on = True
+
+while is_on:
+    choice = input('What would you like? (espresso/latte/cappuccino) ')
+    if choice == 'off':
+        is_on = False
